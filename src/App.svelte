94805fa2f63bf25677d27854007cfb7fe8d885e9
Renato <renatoperezc@gmail.com>
2022-11-27 12:58:43 -0600

    Set up tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
-<main>
+<main class="w-screen h-screen flex flex-col items-center">
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
     </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
 
-  <div class="card">
+  <div class="text-md text-red-500">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>,
+    the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,16 @@
-import sveltePreprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
 
-export default {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: sveltePreprocess()
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+};
+
+export default config;
